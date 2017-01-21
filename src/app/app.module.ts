5c9acca11272cfbca3c174bcab6c5242ecaef1bf
Ryan Cornillie <cornil15@msu.edu>
2017-01-21 13:12:50 -0500

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,41 @@
-import {NgModule, ErrorHandler} from '@angular/core';
-import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
-
-import {MyApp} from './app.component';
-import {AboutPage} from '../pages/about/about';
-import {ContactPage} from '../pages/contact/contact';
-import {HomePage} from '../pages/home/home';
-import {TabsPage} from '../pages/tabs/tabs';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 import { Storage } from '@ionic/storage';
-
 import {FBAuthService} from '../providers/fb-auth-service';
 import {HttpClient} from '../providers/http-client';
 import {UserData} from '../providers/user-data';
 
+
 @NgModule({
-    declarations: [
-        MyApp,
-        AboutPage,
-        ContactPage,
-        HomePage,
-        TabsPage
-    ],
-    imports: [
-        IonicModule.forRoot(MyApp)
-    ],
-    bootstrap: [IonicApp],
-    entryComponents: [
-        MyApp,
-        AboutPage,
-        ContactPage,
-        HomePage,
-        TabsPage
-    ],
-    providers: [UserData, Storage, FBAuthService, HttpClient, {provide: ErrorHandler, useClass: IonicErrorHandler}]
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage
+  ],
+  providers: [UserData, Storage, FBAuthService, HttpClient, {provide: ErrorHandler, useClass: IonicErrorHandler}]
+
 })
 export class AppModule {
 }
