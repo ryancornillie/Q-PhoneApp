7b31ede8a868fb3f855da7fae59d20ff46c1f2ca
Zachary Rosenthal <rosent76@msu.edu>
2017-01-22 03:13:33 -0500

    refresh

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import {Office} from '../../models/Office';
 
 import {UserData} from '../../providers/user-data';
 import {DataProvider} from '../../providers/data-provider';
+import {setDOM} from "@angular/platform-browser/src/dom/dom_adapter";
 
 
 @Component({
@@ -17,42 +18,54 @@ export class HomePage {
     offices: Office[];
     userId: string;
 
-    constructor(public userData: UserData, public dataProvider: DataProvider) {}
+    constructor(public userData: UserData, public dataProvider: DataProvider) {
+    }
 
     ionViewDidLoad() {
 
         this.setData();
     }
 
+    doRefresh(refresher) {
+        console.log('Begin async operation', refresher);
+
+        this.setData().then(
+            success => refresher.complete(),
+            error => refresher.cancel()
+        );
+    }
+
 
     setData() {
-        this.userData.getName().then(
-            (value) => {
-                this.uName = value;
-            },
-            err => err
-        );
 
-        this.userData.getPictureUrl().then(
-            (value) => {
-                this.pictureUrl = value;
-            },
-            err => err
-        );
+        let promises = [
+            this.userData.getName().then(
+                (value) => {
+                    this.uName = value;
+                },
+                err => err
+            ),
+            this.userData.getPictureUrl().then(
+                (value) => {
+                    this.pictureUrl = value;
+                },
+                err => err
+            ),
+            this.userData.getUserId().then(
+                (value) => {
+                    this.userId = value;
+                },
+                err => err
+            ),
+            this.dataProvider.getOffices().then(
+                (success) => {
+                    this.offices = success;
+                },
+                err => err
+            )
+        ];
 
-        this.userData.getUserId().then(
-            (value) => {
-                this.userId = value;
-            },
-            err => err
-        );
-
-        this.dataProvider.getOffices().then(
-            (success) => {
-                this.offices = success;
-            },
-            err => err
-        );
+        return Promise.all(promises);
 
     }
 
